47390bbd7b41d6b113c7db68fc304a12dc8f50dc
HibaSrouji <64808362+HibaSrouji@users.noreply.github.com>
2020-10-05 23:09:17 +0300

    Add files via upload

---
+++ HibaSrouji_PaperAirplane homework_ week1.py
@@ -0,0 +1,14 @@
+# homework Week 1
+"""
+Paoer Airplane homework
+created in Sep/21
+by : Hiba Srouji.
+
+"""
+hello = "Hello All, This is a Paper Airplane homework" 
+print(hello)
+
+x = input ('please inter the tempreture in Celisus')
+y = float(x)
+z = y * 1.8 + 32
+print("the tempreture in Frenheit is " + str(z))
